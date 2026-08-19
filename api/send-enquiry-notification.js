b0824ac9e5e5e88dccc0de518babfeaebadbc4e5
allydochealth <allydoc.health@gmail.com>
2026-08-19 18:07:25 +0100

    Fix admin notification email address

--- api/send-enquiry-notification.js
+++ api/send-enquiry-notification.js
@@ -1,7 +1,7 @@
 import { Resend } from 'resend'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
-const ADMIN_EMAIL = 'sevenlifeuk@gmail.com'
+const ADMIN_EMAIL = 'travelbxp@gmail.com'
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
